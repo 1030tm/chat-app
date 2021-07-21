c28332249a780eb8bd73102cc96a9aa8d07f0e5b
1030tm <moninoki0139@gmail.com>
2021-07-21 14:20:38 +0900

    トップページからの画面遷移しないエラーの修正

--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(version: 2021_07_21_000200) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["room_id"], name: "index_messsages_on_room_id"
-    t.index ["user_id"], name: "index_messsages_on_user_id"
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
